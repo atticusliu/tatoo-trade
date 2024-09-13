3982e5bd2b0281b3f72af9ff3d153dcebbf73f21
Atticus Liu <atticus@trydrippi.com>
2024-09-12 18:02:42 -0700

    cleanup

--- components/CreateProductButton.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function CreateProductButton() {
-  return (
-    <Link href="/products/create" className="hover:text-gray-300">
-      Sell
-    </Link>
-  );
-}
