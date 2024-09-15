5867800c6a993a71361f83042bccab0da99b0a72
Atticus Liu <atticus@trydrippi.com>
2024-09-14 18:33:02 -0700

    add prisma as a dependency

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,6 @@ export default async function Index() {
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
         <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {/* {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />} */}
         </main>
       </div>
 
